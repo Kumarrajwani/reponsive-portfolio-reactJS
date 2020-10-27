20a759194c7acc37e9d5ff2cf6d43c952c257881
Kumarrajwani <meshrajwani@live.com>
2020-10-27 23:27:37 +0100

    personal reactJS portfolio

---
+++ kumar-portfolio/src/Sdata.jsx
@@ -0,0 +1,35 @@
+import software from "../src/images/software.jpeg";
+import app from "../src/images/app.jpg";
+import web from "../src/images/web.jpg";
+import android from "../src/images/android.png";
+import ios from "../src/images/ios.png";
+import digital from "../src/images/digital.jpeg";
+
+const Sdata = [
+  {
+    imgsrc: web,
+    title: "Web development",
+  },
+  {
+    imgsrc: app,
+    title: "App development",
+  },
+  {
+    imgsrc: software,
+    title: "Software development",
+  },
+  {
+    imgsrc: android,
+    title: "Android development",
+  },
+  {
+    imgsrc: ios,
+    title: "IOS development",
+  },
+  {
+    imgsrc: digital,
+    title: "Digital Marketing",
+  },
+];
+
+export default Sdata;
