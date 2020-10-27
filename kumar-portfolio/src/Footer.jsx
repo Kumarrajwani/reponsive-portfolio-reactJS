5dcbd979cc9d2d2a9817da626cf77f05e0e93710
Kumarrajwani <meshrajwani@live.com>
2020-10-28 00:29:23 +0100

    implemented footer

---
+++ kumar-portfolio/src/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="w-100 bg-light text-center">
+        <p>
+          &copy; 2020 RajwaniTechnical. All Rights Reserved | Terms and
+          Conditions
+        </p>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
